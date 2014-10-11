eff5d0c50eec2ac19708f958ce7f9f2d184bff09
Keith Crum <kcrum3@gmail.com>
2014-10-11 13:37:57 -0500

    Added code to submit answer to kaggle.

--- simple_forest.py
+++ simple_forest.py
@@ -7,6 +7,26 @@ from keith_nnclass import scorechart
 # What fraction of fulltrain do you want to be your test set?
 percentCV = 0.3
 
+# This trains on fulltrain and produces a CSV prediction file for the test set.
+def submit():
+    fulltrain = pd.read_csv('dat/train.csv')
+    trainx = fulltrain.drop(['Id','Cover_Type'], axis=1) # Features
+    trainy = fulltrain['Cover_Type'] # Target
+    
+    fulltest = pd.read_csv('dat/test.csv')
+    testid = fulltest['Id'] # You'll need to stitch these onto the predictions
+    testx = fulltest.drop(['Id'],axis=1) # Features
+
+    # Create and fit SVM
+    clf = ensemble.RandomForestClassifier()
+    clf.fit(trainx, trainy)
+    
+    # Now predict test set
+    ypred = clf.predict(testx)
+    # Write prediction to file with Kaggle-approved format
+    output = pd.Series(ypred, index=testid, name='Cover_Type')
+    output.to_csv('output/simple_forest_output.csv', header=True)
+    
 # Main
 def main():
     fulltrain = pd.read_csv('dat/train.csv')
@@ -27,5 +47,5 @@ def main():
 
 #####
 if __name__ == '__main__':
-    main()
-
+    #main()
+    submit()
